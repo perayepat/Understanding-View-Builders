899ed168fe21f30cb8ed33ac97c932cb61e210f5
Mphathi Maapola <113903905+Patglu@users.noreply.github.com>
2023-03-12 17:51:30 +0200

    Passing in a view instead of using a view builder

--- Understanding View Builders/Basics.swift
+++ Understanding View Builders/Basics.swift
@@ -18,7 +18,22 @@ struct Basics: View {
 /// Small veiw structs design principles
 extension Basics{
    @ViewBuilder func squareOrCircle() -> some View{
-        
+        if isSquare{
+            RoundedRectangle(cornerRadius: 10)
+                .fill(.green)
+                .frame(width: 100, height: 100)
+        } else {
+            Circle()
+                .fill(.red)
+                .frame(width: 100)
+        }
+    }
+}
+
+//MARK: - Using a view as a seperate function
+struct SquareOrCirle: View{
+    @Binding var isSquare: Bool
+    var body: some View{
         if isSquare{
             RoundedRectangle(cornerRadius: 10)
                 .fill(.green)
