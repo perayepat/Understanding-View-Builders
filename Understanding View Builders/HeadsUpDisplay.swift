4378958adc02e577eb84d6234fb8b56bb7a23e5d
Mphathi Maapola <113903905+Patglu@users.noreply.github.com>
2023-03-12 18:41:03 +0200

    HUD display with its own custom animation and binding properties

--- Understanding View Builders/HeadsUpDisplay.swift
+++ Understanding View Builders/HeadsUpDisplay.swift
@@ -1,13 +1,24 @@
 import SwiftUI
 
 struct HeadsUpDisplay: View {
+    @State private var isShowingHUD = false
     var body: some View {
         NavigationView{
-            HUDView {
-                Text("Heads Up Display")
+            ZStack{
+                if isShowingHUD{
+                    HUDView(isShowingHUD: $isShowingHUD){
+                        Text("Heads Up Display")
+                    }
+                }
+                ZStack{
+                    Color(.green)
+                        Button("Show HUD"){
+                            isShowingHUD.toggle()
+                        }
+                    
+                }
             }
-                    .navigationTitle("Heads Up Display")
-            
+            .navigationTitle("Heads Up Display")
         }
     }
 }
@@ -21,8 +32,10 @@ struct HeadsUpDisplay_Previews: PreviewProvider {
 
 struct HUDView<Content: View>: View{
     let content: Content
-    init(@ViewBuilder content: () -> Content) {
+    @Binding var isShowingHUD: Bool
+    init(isShowingHUD: Binding<Bool>,@ViewBuilder content: () -> Content) {
         self.content = content()
+        self._isShowingHUD = isShowingHUD
     }
     
     var body: some View{
@@ -31,6 +44,16 @@ struct HUDView<Content: View>: View{
                 content
             }
             Spacer()
+        }
+        .zIndex(1) ///Making it the first view in a stack
+        .transition(.move(edge: .top))
+        .animation(.spring(),value: isShowingHUD)
+        .onAppear{
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3){
+                withAnimation {
+                    isShowingHUD = false
+                }
+            }
         }
     }
 }
